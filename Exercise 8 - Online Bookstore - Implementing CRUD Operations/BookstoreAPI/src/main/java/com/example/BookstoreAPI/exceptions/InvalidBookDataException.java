1adf99da81d7534df29b93c21ee2189cfc22e6f9
anmol1205 <91623229+anmol1205@users.noreply.github.com>
2024-08-27 16:45:44 +0530

    Delete Exercise 8 - Online Bookstore - Implementing CRUD Operations/BookstoreAPI directory

--- Exercise 8 - Online Bookstore - Implementing CRUD Operations/BookstoreAPI/src/main/java/com/example/BookstoreAPI/exceptions/InvalidBookDataException.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookstoreAPI.exceptions;
-
-public class InvalidBookDataException extends RuntimeException{
-	public InvalidBookDataException(String message) {
-        super(message);
-    }
-}
